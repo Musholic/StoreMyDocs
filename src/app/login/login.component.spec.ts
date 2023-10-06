6fc163de4d3adfb07c6240d5fc3863804bfb72cd
Musholic <musholic.tbg@gmail.com>
2023-10-06 10:04:13 +0200

    [us14] On the login page, check if the user needs to be authenticated or if he needs to be authorized

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,12 +1,49 @@
 import {LoginComponent} from './login.component';
-import {MockBuilder, MockRender} from "ng-mocks";
+import {MockBuilder, MockInstance, MockRender, ngMocks} from "ng-mocks";
 import {AppModule} from "../app.module";
+import {GoogleDriveAuthService} from "../file-upload/google-drive-auth.service";
+import {mock, when} from "strong-mock";
 
 describe('LoginComponent', () => {
+  MockInstance.scope();
+
   beforeEach(() => MockBuilder(LoginComponent, AppModule));
 
   it('should create', () => {
     let component = MockRender(LoginComponent).point.componentInstance;
     expect(component).toBeTruthy();
   });
+
+  it('should ask for authentication when the user is not authenticated', () => {
+    // Arrange
+    let isAuthenticatedMock = MockInstance(GoogleDriveAuthService, 'isAuthenticated', mock<GoogleDriveAuthService['isAuthenticated']>());
+    when(() => isAuthenticatedMock()).thenReturn(false).atLeast(1);
+    MockRender(LoginComponent);
+
+    // Act
+    let message = Page.getMessage();
+
+    // Assert
+    expect(message).toEqual('You must be logged in to use this application')
+  })
+
+  it('should ask for authorization if the user does not have a valid API token', () => {
+    // Arrange
+    let isAuthenticatedMock = MockInstance(GoogleDriveAuthService, 'isAuthenticated', mock<GoogleDriveAuthService['isAuthenticated']>());
+    when(() => isAuthenticatedMock()).thenReturn(true).atLeast(1);
+    MockRender(LoginComponent);
+
+    // Act
+    let message = Page.getMessage();
+
+    // Assert
+    expect(message).toEqual('You need to authorize the access to your Google Drive files. ' +
+      'Only the files and folders that you open or create with this application will be accessible')
+  })
 });
+
+class Page {
+  static getMessage(): string {
+    return ngMocks.find('.login-message').nativeNode.textContent.trim();
+  }
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {GoogleDriveAuthService} from "../file-upload/google-drive-auth.service";
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private authService: GoogleDriveAuthService) {
+  }
+
+  isAuthenticated() {
+    return this.authService.isAuthenticated();
+  }
 }
