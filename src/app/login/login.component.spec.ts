ee7a5f411e3aa500d5f48bf474269d0db0c23cac
Musholic <musholic.tbg@gmail.com>
2023-10-06 10:46:17 +0200

    [us14] On the login page, add an authorize button to request authorization

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -3,6 +3,11 @@ import {MockBuilder, MockInstance, MockRender, ngMocks} from "ng-mocks";
 import {AppModule} from "../app.module";
 import {GoogleDriveAuthService} from "../file-upload/google-drive-auth.service";
 import {mock, when} from "strong-mock";
+import {HarnessLoader} from "@angular/cdk/testing";
+import {ComponentFixture} from "@angular/core/testing";
+import {TestbedHarnessEnvironment} from "@angular/cdk/testing/testbed";
+import {MatButtonHarness} from "@angular/material/button/testing";
+import {Router} from "@angular/router";
 
 describe('LoginComponent', () => {
   MockInstance.scope();
@@ -27,23 +32,56 @@ describe('LoginComponent', () => {
     expect(message).toEqual('You must be logged in to use this application')
   })
 
-  it('should ask for authorization if the user does not have a valid API token', () => {
-    // Arrange
-    let isAuthenticatedMock = MockInstance(GoogleDriveAuthService, 'isAuthenticated', mock<GoogleDriveAuthService['isAuthenticated']>());
-    when(() => isAuthenticatedMock()).thenReturn(true).atLeast(1);
-    MockRender(LoginComponent);
+  describe('When the user does not have a valid API token', () => {
+    it('should ask for authorization', () => {
+      // Arrange
+      let isAuthenticatedMock = MockInstance(GoogleDriveAuthService, 'isAuthenticated', mock<GoogleDriveAuthService['isAuthenticated']>());
+      when(() => isAuthenticatedMock()).thenReturn(true).atLeast(1);
+      MockRender(LoginComponent);
 
-    // Act
-    let message = Page.getMessage();
+      // Act
+      let message = Page.getMessage();
 
-    // Assert
-    expect(message).toEqual('You need to authorize the access to your Google Drive files. ' +
-      'Only the files and folders that you open or create with this application will be accessible')
+      // Assert
+      expect(message).toEqual('You need to authorize the access to your Google Drive files. ' +
+        'Only the files and folders that you open or create with this application will be accessible')
+    })
+
+    it('should authorize and then redirect when clicking on authorize button', async () => {
+      // Arrange
+      let isAuthenticatedMock = MockInstance(GoogleDriveAuthService, 'isAuthenticated', mock<GoogleDriveAuthService['isAuthenticated']>());
+      when(() => isAuthenticatedMock()).thenReturn(true).atLeast(1);
+
+      // Expect the api token to be requested
+      let getApiTokenMock = MockInstance(GoogleDriveAuthService, 'getApiToken', mock<GoogleDriveAuthService['getApiToken']>());
+      when(() => getApiTokenMock()).thenResolve('apiToken');
+
+      // Expect a redirection
+      let navigateByUrlMock = MockInstance(Router, 'navigateByUrl', mock<Router['navigateByUrl']>());
+      when(() => navigateByUrlMock('/')).thenResolve(true)
+
+      let fixture = MockRender(LoginComponent);
+      let page = new Page(fixture);
+
+      // Act
+      await page.clickAuthorize();
+    })
   })
 });
 
 class Page {
+  private loader: HarnessLoader;
+
+  constructor(fixture: ComponentFixture<LoginComponent>) {
+    this.loader = TestbedHarnessEnvironment.loader(fixture);
+  }
+
   static getMessage(): string {
     return ngMocks.find('.login-message').nativeNode.textContent.trim();
+  }
+
+  async clickAuthorize() {
+    let button = await this.loader.getHarness(MatButtonHarness.with({text: 'Authorize'}));
+    return button.click();
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {GoogleDriveAuthService} from "../file-upload/google-drive-auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -8,10 +9,15 @@ import {GoogleDriveAuthService} from "../file-upload/google-drive-auth.service";
 })
 export class LoginComponent {
 
-  constructor(private authService: GoogleDriveAuthService) {
+  constructor(private authService: GoogleDriveAuthService, private router: Router) {
   }
 
   isAuthenticated() {
     return this.authService.isAuthenticated();
+  }
+
+  async authorize() {
+    await this.authService.getApiToken();
+    return this.router.navigateByUrl('/');
   }
 }
